0dfe2b50a3e586aefce12fdf13e83eabe2caf457
vldmrvlsk <vlasyuk@edu.surgu.ru>
2022-12-01 15:54:03 +0500

    working now

--- catalog.php
+++ catalog.php
@@ -4,4 +4,4 @@ $result = $conn->query("SELECT * FROM products");
 require('components/header.php');
 require('components/products_list.php');
 require('components/footer.php');
-
+?>
--- components/products_list.php
+++ components/products_list.php
@@ -4,15 +4,19 @@
         <ul class="products-list">
 
             <?php while ($item = $result->fetch()) {?>
-            <li>
-                <a class="products-card" href="product.php?product_id=<?echo $item['id'] ?>">
-                    <h3><?= $item['name'] ?></h3>
-                    <p><?= $item['description'] ?></p>
-                    <span class="price"><del><?= $item['price'] ?></del><?= $item['price'] ?></span>
-                    <img src=""<?= $item['image_url'] ?>" widht="156" height="120" alt="<?= $item['name'] ?> ">
-                </a>
-            </li>
+                <li>
+                    <a class="product-card" href="product.php?product_id=<?echo $item['id'] ?>">
+                        <h3><?= $item['name'] ?></h3>
+                        <p><?= $item['description'] ?></p>
+                        <span class="price"><del><?= $item['price'] ?></del> <?= $item['price'] ?></span>
+                        <img src="<?= $item['image_url'] ?>" width="156" height="120" alt="<?= $item['title'] ?>">
+                    </a>
+                </li>
+
+
             <?php }; ?>
+
+
         </ul>
     </div>
 </section>
