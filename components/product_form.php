8cb786a53e8c02e9b7e44c7eada77150627559f9
vldmrvlsk <vlasyuk@edu.surgu.ru>
2023-04-07 21:39:12 +0500

    script is working

--- components/product_form.php
+++ components/product_form.php
@@ -19,7 +19,34 @@
                 <input type="file" name="picture[]" id="id4" multiple>
             </p>
             <p>
-            <p><input type="submit" value="Создать"></p>
+            <p><input type="button" value="Создать" onclick="validate(this.form)"></p>
         </form>
     </div>
 </section>
+<script>
+    function validate(xxx) {
+        var formData = new FormData(xxx);
+        if (xxx.name.value === '')
+            alert("Вы не ввели наименование товара!");
+        else if (xxx.description.value === '')
+            alert("Добавьте описание товара, пожалуйста!");
+        else if (!Number.isInteger(parseInt(xxx.price.value)) || (String(parseInt(xxx.price.value)) !== xxx.price.value) || parseInt(xxx.price.value)<0)
+            alert("Цена должна быть целым положительным числом!");
+        // else if (!formData.has("picture[]"))
+        //     alert("Вы не добавили изображение");
+        else {
+            var file = formData.get("picture[]");
+            if (!file.size) {
+                alert("Вы не добавили изображение");
+                return;
+            }
+            var fileType = file.type;
+            if (fileType !== 'image/jpeg' && fileType !== 'image/png') {
+                alert("Файл не является форматом JPG или PNG!");
+                return;
+            }
+            alert("Поздравляю! Вы успешно добавили товар!");
+            xxx.submit();
+        }
+    }
+</script>
